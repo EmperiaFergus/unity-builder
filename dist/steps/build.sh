082010904e7ed2c40857d919ed95342eead99143
EmperiaFergus <Fergus@emperia.co.uk>
2021-08-02 14:46:29 +0100

    update build.sh

--- dist/steps/build.sh
+++ dist/steps/build.sh
@@ -123,7 +123,7 @@ echo "###########################"
 $s3InstallerPath=C:/Repositories/unity-builder/builder/default-build-script/ProjectSettings/S3Installer.asset
 
 $s3InstallerFinalPath=C:/S3Installer.asset
-serverInstallerPath="${UNITY_PROJECT_PATH}/Assets/GameAssets/Resources/Installers/S3Installer.asset"
+$serverInstallerPath="${UNITY_PROJECT_PATH}/Assets/GameAssets/Resources/Installers/S3Installer.asset"
 $serverInstallerFinalPath=C:/ServerInstaller.asset
 
 # /Assets/GameAssets/Resources/Installers
