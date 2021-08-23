52cec6de708d8a6264f0f0c53e85d4dadf9a55e4
EmperiaFergus <Fergus@emperia.co.uk>
2021-08-23 17:20:05 +0100

    update build.sh

--- dist/steps/build.sh
+++ dist/steps/build.sh
@@ -178,6 +178,7 @@ if [[ ${BUILD_TARGET} != *"OSX"* ]];then
 else
   echo "building for OSX"
   sed -i 's|selectedDefinition: Win64|'"selectedDefinition: OSX"'|g' ${UNITY_PROJECT_PATH}/Assets/SIDGIN/EditorResources/StandaloneBuildSettingsData.asset
+  echo "$(${UNITY_PROJECT_PATH}/Assets/SIDGIN/EditorResources/StandaloneBuildSettingsData.asset)"
 fi
 
 #this is required as we get a error about the build function not existing even when it's correct
