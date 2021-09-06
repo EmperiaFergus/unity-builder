a4df866056f622a5aad62d4f083bb245f7bf3c93
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-06 12:22:03 +0100

    update remote-builder.ts

--- src/model/remote-builder/remote-builder.ts
+++ src/model/remote-builder/remote-builder.ts
@@ -267,6 +267,14 @@ class RemoteBuilder {
           value: buildParameters.serverTimeout,
         },
         {
+          name: 'patchkitAPIKey',
+          value: buildParameters.patchkitAPIKey,
+        },
+        {
+          name: 'patchkitSecret',
+          value: buildParameters.patchkitSecret,
+        },
+        {
           name: 'BUILD_TARGET',
           value: buildParameters.platform,
         },
