1652ac8c678b86e02e650b3462a57b34956a58cf
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-07 10:25:05 +0100

    update build.sh

--- dist/steps/build.sh
+++ dist/steps/build.sh
@@ -276,21 +276,22 @@ ls -alh "$BUILD_PATH_FULL"
 echo "###########################"
 echo "#         Patchkit        #"
 echo "###########################"
-#find / -type d -print
-#cd /github/workspace/dist/patchkit
-#ls -a
-#./patchkit-tools
-#patchkit-tools make-version -s $patchkitSecret -a $patchkitAPIKey -l "test upload" -f $BUILD_PATH_FULL
+find / -type d -print
+cd /github/workspace/dist/patchkit
+ls -a
+./patchkit-tools
+RUN ["chmod", "+x", "/github/workspace/dist/patchkit/patchkit-tools"]
+patchkit-tools make-version -s $patchkitSecret -a $patchkitAPIKey -l "test upload" -f $BUILD_PATH_FULL
 
-set -e
+#set -e
 
-# Figure out where this script is located.
-SELFDIR="/github/workspace/dist/patchkit"
-SELFDIR="`cd \"$SELFDIR\" && pwd`"
-
-# Tell Bundler where the Gemfile and gems are.
-export BUNDLE_GEMFILE="$SELFDIR/vendor/Gemfile"
-unset BUNDLE_IGNORE_CONFIG
-
-# Run the actual app using the bundled Ruby interpreter, with Bundler activated.
-exec "$SELFDIR/ruby/bin/ruby" -rbundler/setup "$SELFDIR/app/core/bootstrap.rb" "$@"
+## Figure out where this script is located.
+#SELFDIR="/github/workspace/dist/patchkit"
+#SELFDIR="`cd \"$SELFDIR\" && pwd`"
+#
+## Tell Bundler where the Gemfile and gems are.
+#export BUNDLE_GEMFILE="$SELFDIR/vendor/Gemfile"
+#unset BUNDLE_IGNORE_CONFIG
+#
+## Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+#exec "$SELFDIR/ruby/bin/ruby" -rbundler/setup "$SELFDIR/app/core/bootstrap.rb" "$@"
