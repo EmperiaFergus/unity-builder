6b4576f9e7bca22e32213e8b1760bb7d7974aab4
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-07 09:17:07 +0100

    update build.sh

--- dist/steps/build.sh
+++ dist/steps/build.sh
@@ -279,4 +279,20 @@ echo "###########################"
 #find / -type d -print
 cd /github/workspace/dist/patchkit
 patchkit-tools.sh
+
+set -e
+
+# Figure out where this script is located.
+SELFDIR="`/github/workspace/dist/patchkit`"
+SELFDIR="`cd \"$SELFDIR\" && pwd`"
+
+# Tell Bundler where the Gemfile and gems are.
+export BUNDLE_GEMFILE="$SELFDIR/vendor/Gemfile"
+unset BUNDLE_IGNORE_CONFIG
+
+# Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+exec "$SELFDIR/ruby/bin/ruby" -rbundler/setup "$SELFDIR/app/core/bootstrap.rb" "$@"
+
 patchkit-tools make-version -s $patchkitSecret -a $patchkitAPIKey -l "test upload" -f $BUILD_PATH_FULL
+
+
