ed744c66d46b051efd33b310091edf0ed6b01641
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-07 11:51:18 +0100

    move patchkit to seperate file

---
+++ dist/steps/publish.sh
@@ -0,0 +1,22 @@
+echo "###########################"
+echo "#         Patchkit        #"
+echo "###########################"
+find / -type d -print
+cd /github/workspace/dist/patchkit
+ls -a
+ ./patchkit-tools
+#chmod, +x, ./patchkit-tools
+patchkit-tools make-version -s $patchkitSecret -a $patchkitAPIKey -l "test upload" -f $BUILD_PATH_FULL
+
+#set -e
+
+## Figure out where this script is located.
+#SELFDIR="/github/workspace/dist/patchkit"
+#SELFDIR="`cd \"$SELFDIR\" && pwd`"
+#
+## Tell Bundler where the Gemfile and gems are.
+#export BUNDLE_GEMFILE="$SELFDIR/vendor/Gemfile"
+#unset BUNDLE_IGNORE_CONFIG
+#
+## Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+#exec "$SELFDIR/ruby/bin/ruby" -rbundler/setup "$SELFDIR/app/core/bootstrap.rb" "$@"
