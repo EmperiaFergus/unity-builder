aabef71488dec64631a981d4430213980f0d176d
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-10 17:27:45 +0100

    update publish.sh

--- dist/steps/publish.sh
+++ dist/steps/publish.sh
@@ -1,10 +1,21 @@
 echo "###########################"
 echo "#         Patchkit        #"
 echo "###########################"
-find / -type d -print
+#find / -type d -print
 cd ./test-project/Assets/Builders/patchkit/
+echo 1.
+echo $patchkitSecret
+echo 2.
+echo $patchkitAPIKey
+echo 3.
+echo $BUILD_PATH_FULL
+
+for entry in "$search_dir"/*
+do
+  echo "$entry"
+done
 #chmod, +x, ./patchkit-tools
-patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test2 -f $BUILD_PATH_FULL -x
+patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 
 #set -e
 
