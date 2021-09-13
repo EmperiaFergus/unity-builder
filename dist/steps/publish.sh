2e310618982df0cc5e5715cab4c22784014860cc
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-13 09:55:46 +0100

    publish.sh

--- dist/steps/publish.sh
+++ dist/steps/publish.sh
@@ -1,7 +1,7 @@
 echo "###########################"
 echo "#         Patchkit        #"
 echo "###########################"
-find / -type d -print
+#find / -type d -print
 echo 1.
 echo $patchkitSecret
 echo 2.
@@ -16,5 +16,6 @@ done
 #update path to where new path is
 #get access to patchkit
 #./test-project/Assets/Builders/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
-./patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
+/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
+/github/workspace/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 
