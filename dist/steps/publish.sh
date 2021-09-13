9061969af86f8539b9b5e830b711c34cfb845a56
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-13 15:12:02 +0100

    revert dockerfile, update publish path

--- dist/steps/publish.sh
+++ dist/steps/publish.sh
@@ -20,7 +20,7 @@ done
 # RUN chmod -R +x /patchkit
 #/github/workspace/dist/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 
-
-/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
+cd /patchkit
+patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 
 
