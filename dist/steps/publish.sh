6e18975cd03a717109bf4983d6c90824e33f4c7f
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-13 10:50:10 +0100

    add patchkit to Dockerfile

--- dist/steps/publish.sh
+++ dist/steps/publish.sh
@@ -16,6 +16,5 @@ done
 #update path to where new path is
 #get access to patchkit
 #./test-project/Assets/Builders/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
-/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 /github/workspace/patchkit/patchkit-tools.sh make-version -s $patchkitSecret -a $patchkitAPIKey -l test3 -f $BUILD_PATH_FULL -x
 
